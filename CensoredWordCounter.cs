47725a87b7f2e851005dad1630f7706dbb0f058c
pascal-ene-96 <pascal_ene@hotmail.co.uk>
2017-09-07 17:03:52 +0100

    Add files via upload

---
+++ CensoredWordCounter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Words
+{
+    public class CensoredWordCounter
+    {
+        // Task 3 - Part A
+
+
+        public static int CensoredWordCount(String sentence)
+        {
+            Console.WriteLine("Your sentence is " + "\"" + sentence + "\"");
+            Console.WriteLine(" ");
+
+            string[] censorCount = { "dog", "cat", "large" };
+
+            foreach (String word in censorCount)
+            {
+                Console.WriteLine("Censored word: " + word);
+            }
+
+            int counts = 0;
+            int total = 0;
+
+            Console.WriteLine("");
+
+            foreach (String item in censorCount)
+            {
+                foreach (Match match in Regex.Matches(sentence, item))
+                {
+                    counts++;
+                }
+
+                Console.WriteLine("{0}" + " Found " + "{1}" + " Times", item, Regex.Matches(sentence, item).Count);
+                total = total + Regex.Matches(sentence, item).Count;
+
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Total: " + total);
+            return total;
+
+        }
+    }
+}
